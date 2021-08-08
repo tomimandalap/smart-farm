fb8305b74eee440208aa750b5419980b0cb99500
Tomi Mandala Putra <tommymandalaputra@gmail.com>
2021-08-08 12:19:43 +0700

    style: add variant color

--- src/views/Monitoring.vue
+++ src/views/Monitoring.vue
@@ -8,39 +8,49 @@
           Realtime Data
         </h2>
         <v-row>
-          <v-col lg="4" md="4" sm="12" cols="12">
+          <v-col
+            v-for="(items, i) in sensor"
+            :key="i"
+            lg="4"
+            md="4"
+            sm="12"
+            cols="12"
+          >
             <v-card elevation="0" outlined class="mt-10 text-center">
-              <v-card-title class="d-block mx-auto f-xl">
-                Temperature
+              <v-card-title
+                :class="
+                  i == 0
+                    ? 'blue--text d-block mx-auto f-xl'
+                    : i == 1
+                    ? 'green--text d-block mx-auto f-xl'
+                    : 'orange--text d-block mx-auto f-xl'
+                "
+              >
+                {{ items.label }}
               </v-card-title>
-              <v-card-text class="f-card-text my-6 black--text">
-                30.00&deg;C
+              <v-card-text
+                :class="
+                  i == 0
+                    ? 'blue--text f-card-text my-6'
+                    : i == 1
+                    ? 'green--text f-card-text my-6'
+                    : 'orange--text f-card-text my-6'
+                "
+              >
+                {{ items.value[items.value.length - 1]
+                }}{{ i == 0 ? '&deg;C' : '%' }}
               </v-card-text>
-              <v-card-subtitle>Updated 1 minutes ago</v-card-subtitle>
-            </v-card>
-          </v-col>
-
-          <v-col lg="4" md="4" sm="12" cols="12">
-            <v-card elevation="0" outlined class="mt-10 text-center">
-              <v-card-title class="d-block mx-auto f-xl">
-                Humidity
-              </v-card-title>
-              <v-card-text class="f-card-text my-6 black--text">
-                60.50%
-              </v-card-text>
-              <v-card-subtitle>Updated 1 minutes ago</v-card-subtitle>
-            </v-card>
-          </v-col>
-
-          <v-col lg="4" md="4" sm="12" cols="12">
-            <v-card elevation="0" outlined class="mt-10 text-center">
-              <v-card-title class="d-block mx-auto f-xl">
-                Soil Moisture
-              </v-card-title>
-              <v-card-text class="f-card-text my-6 black--text">
-                80.25%
-              </v-card-text>
-              <v-card-subtitle>Updated 1 minutes ago</v-card-subtitle>
+              <v-card-subtitle
+                :class="
+                  i == 0
+                    ? 'blue--text'
+                    : i == 1
+                    ? 'green--text'
+                    : 'orange--text'
+                "
+              >
+                Updated 1 minutes ago
+              </v-card-subtitle>
             </v-card>
           </v-col>
         </v-row>
@@ -54,21 +64,33 @@
         </h2>
         <v-row>
           <v-col
-            v-for="item in 3"
-            :key="item"
+            v-for="(items, i) in sensor"
+            :key="i"
             lg="12"
             md="12"
             sm="12"
             cols="12"
             class="mb-5"
           >
-            <v-card elevation="0" outlined>
-              <v-card-title>Temperature 30.00&deg;C</v-card-title>
+            <v-card elevation="0">
+              <v-card-title
+                :class="
+                  i == 0
+                    ? 'blue--text'
+                    : i == 1
+                    ? 'green--text'
+                    : 'orange--text'
+                "
+              >
+                {{ items.label }}
+                {{ items.value[items.value.length - 1]
+                }}{{ i == 0 ? '&deg;C' : '%' }}
+              </v-card-title>
               <v-card-text>
                 <v-sheet color="white">
                   <v-sparkline
-                    :value="value"
-                    color="black"
+                    :value="items.value"
+                    :color="i == 0 ? 'blue' : i == 1 ? 'green' : 'orange'"
                     height="100"
                     padding="16"
                     line-width="2"
@@ -97,7 +119,23 @@ export default {
   data: () => ({
     title: 'Monitoring',
     powerby: 'Power by arduino and PCBWay',
-    value: [30.1, 29.2, 30.5, 28.6, 30.0, 29.8],
+    sensor: [
+      {
+        id: 1,
+        label: 'Tempertaute',
+        value: [30.1, 29.2, 30.5, 28.6, 30.0, 29.8],
+      },
+      {
+        id: 2,
+        label: 'Humidity',
+        value: [70.1, 69.2, 70.5, 68.6, 77.0, 75.8],
+      },
+      {
+        id: 3,
+        label: 'Soil Moisture',
+        value: [60.1, 58.2, 30.5, 60.6, 70.0, 65.8],
+      },
+    ],
   }),
 }
 </script>
